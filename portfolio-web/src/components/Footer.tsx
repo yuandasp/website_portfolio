d185201a06270bf5563e3a1f7ef319fa9fc7fe81
yuandasp <sitiyuandap@gmail.com>
2023-08-23 20:26:09 +0700

    add footer

--- portfolio-web/src/components/Footer.tsx
+++ portfolio-web/src/components/Footer.tsx
@@ -2,8 +2,7 @@ function Footer() {
   return (
     <div className="bg-black w-full flex justify-center items-center py-4">
       <p className="color-text-blue text-sm md:text-base text-center mx-4">
-        Built with Next.js and Tailwind CSS, deployed with Vercel. All text is
-        set in the Inter typeface.
+        Built by Yuanda | 2023
       </p>
     </div>
   );
